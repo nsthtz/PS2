fee1ea22a29741c848bc138147186323f0b1298f
nsthtz <rjinswand@gmail.com>
2017-10-02 15:54:47 +0200

    global 2d array!

--- RPS_MPI.c
+++ RPS_MPI.c
@@ -57,113 +57,113 @@ cell** local_petri_B_image;
 
 int main(int argc, char** argv){
 
-  srand(1234);
+	srand(1234);
 
-  // Ask MPI what size (number of processors) and rank (which process we are)
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-
-  ////////////////////////////////
-  // Create cartesian communicator
-  int dims[2];
-  dims[0] = p_x_dims;
-  dims[1] = p_y_dims;
-
-	
-  int periods[2]; // we set these to 0 because we are not interested in wrap-around
-  periods[0] = 0;
-  periods[1] = 0;
-
-  int coords[2];
-  coords[0] = p_my_x_dim;
-  coords[1] = p_my_y_dim;
-
-  MPI_Dims_create(size, 2, dims);
-  MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
-  MPI_Cart_coords(cart_comm, rank, 2, coords);
-
-  MPI_Cart_shift(cart_comm, 0, 1, &p_north, &p_south);
-  MPI_Cart_shift(cart_comm, 1, 1, &p_west, &p_east);
-
-  p_x_dims = dims[0];
-  p_y_dims = dims[1];
-
-  p_my_x_dim = coords[0];
-  p_my_y_dim = coords[1];
-  ////////////////////////////////
-  ////////////////////////////////
-
-  initialize();
+	// Ask MPI what size (number of processors) and rank (which process we are)
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 
-  create_types();
+	////////////////////////////////
+	// Create cartesian communicator
+	int dims[2];
+	dims[0] = p_x_dims;
+	dims[1] = p_y_dims;
+
+
+	int periods[2]; // we set these to 0 because we are not interested in wrap-around
+	periods[0] = 0;
+	periods[1] = 0;
+
+	int coords[2];
+	coords[0] = p_my_x_dim;
+	coords[1] = p_my_y_dim;
+
+	MPI_Dims_create(size, 2, dims);
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
+	MPI_Cart_coords(cart_comm, rank, 2, coords);
+
+	MPI_Cart_shift(cart_comm, 0, 1, &p_north, &p_south);
+	MPI_Cart_shift(cart_comm, 1, 1, &p_west, &p_east);
+
+	p_x_dims = dims[0];
+	p_y_dims = dims[1];
+
+	p_my_x_dim = coords[0];
+	p_my_y_dim = coords[1];
+	////////////////////////////////
+	////////////////////////////////
+
+	initialize();
+
+
+	create_types();
 
 	iterate_CA();
 
-  	for (int i; i < ITERATIONS; i++) {
-	
+	for (int i; i < ITERATIONS; i++) {
+
 		exchange_borders();
 
 		iterate_CA();		
-			
+		
 	}
-  // A super basic example sending some data:
+	// A super basic example sending some data:
 
-  // cell* my_test_cell = malloc(10*sizeof(cell));
-  // for(int ii = 0; ii < 10; ii++){
-  //   my_test_cell[ii].strength = ii;
-  //   my_test_cell[ii].color = rank;
-  // }
+	// cell* my_test_cell = malloc(10*sizeof(cell));
+	// for(int ii = 0; ii < 10; ii++){
+	//   my_test_cell[ii].strength = ii;
+	//   my_test_cell[ii].color = rank;
+	// }
 
-  // if(rank == 0){
-  //   cell* rec_buf = malloc(sizeof(cell)*10);
-  //   for(int ii = 0; ii < size - 1; ii++){
-  //     MPI_Recv(rec_buf, 10, mpi_cell_t, ii+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //     printf("receiving from rank %d: \n", ii+1);
-  //     for(int jj = 0; jj < 10; jj++){
-  //       printf("[%d, %d]  ", rec_buf[jj].color, rec_buf[jj].strength);
-  //     }
-  //     printf("\n");
-  //   }
-  // }
-  // else{
-  //   MPI_Send(my_test_cell, 10, mpi_cell_t, 0, 0, MPI_COMM_WORLD);
-  // }
+	// if(rank == 0){
+	//   cell* rec_buf = malloc(sizeof(cell)*10);
+	//   for(int ii = 0; ii < size - 1; ii++){
+	//     MPI_Recv(rec_buf, 10, mpi_cell_t, ii+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	//     printf("receiving from rank %d: \n", ii+1);
+	//     for(int jj = 0; jj < 10; jj++){
+	//       printf("[%d, %d]  ", rec_buf[jj].color, rec_buf[jj].strength);
+	//     }
+	//     printf("\n");
+	//   }
+	// }
+	// else{
+	//   MPI_Send(my_test_cell, 10, mpi_cell_t, 0, 0, MPI_COMM_WORLD);
+	// }
 
 
-  gather_petri();
+	gather_petri();
 
-  MPI_Finalize();
+	MPI_Finalize();
 
-  if(rank==0){
-    // TODO: Write the petri to an image
-  }
+	if(rank==0){
+	// TODO: Write the petri to an image
+	}
 
-  // You should probably make sure to free your memory here
-  // We will dock points for memory leaks, don't let your hard work go to waste!
-  // free_stuff()
+	// You should probably make sure to free your memory here
+	// We will dock points for memory leaks, don't let your hard work go to waste!
+	// free_stuff()
 
-  exit(0);
+	exit(0);
 }
 
 
 void create_types(){
 
-  ////////////////////////////////
-  ////////////////////////////////
-  // cell type
-  const int    nitems=2;
-  int          blocklengths[2] = {1,1};
-  MPI_Datatype types[2] = {MPI_INT, MPI_INT};
-  MPI_Aint     offsets[2];
+	////////////////////////////////
+	////////////////////////////////
+	// cell type
+	const int    nitems=2;
+	int          blocklengths[2] = {1,1};
+	MPI_Datatype types[2] = {MPI_INT, MPI_INT};
+	MPI_Aint     offsets[2];
 
-  offsets[0] = offsetof(cell, color);
-  offsets[1] = offsetof(cell, strength);
+	offsets[0] = offsetof(cell, color);
+	offsets[1] = offsetof(cell, strength);
 
-  MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_cell_t);
-  MPI_Type_commit(&mpi_cell_t);
+	MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_cell_t);
+	MPI_Type_commit(&mpi_cell_t);
   	////////////////////////////////
   	////////////////////////////////
 
@@ -176,13 +176,15 @@ void create_types(){
 	MPI_Type_commit(&local_petri_t);
 	
 
-	MPI_Type_contiguous(p_local_petri_y_dim, mpi_cell_t, &border_row_t);
-  	MPI_Type_commit(&border_row_t);
+
   	////////////////////////////////
   	////////////////////////////////
 
 
   	//TODO: Create MPI types for border exchange
+
+	MPI_Type_contiguous(p_local_petri_y_dim, mpi_cell_t, &border_row_t);
+  	MPI_Type_commit(&border_row_t);
 
 	MPI_Type_vector(p_local_petri_x_dim, 1, p_local_petri_x_dim, mpi_cell_t, &border_col_t);
 	MPI_Type_commit(&border_col_t);
@@ -193,20 +195,35 @@ void create_types(){
 void initialize(){
 	//TODO: assign the following to something more useful than 0
 	/* Each dimension is increased by 2 to let there be a blank border surrounding the actual petri cells for which to insert information retrieved from border exchange */
+
 	p_local_petri_x_dim = IMG_X / sqrt(size) + 2;
 	p_local_petri_y_dim = IMG_Y / sqrt(size) + 2;
 
 	// TODO: When allocating these buffers, keep in mind that you might need to allocate a little more
 	// than just your piece of the petri.
-	local_petri_A = malloc((p_local_petri_x_dim * p_local_petri_y_dim)*sizeof(cell)); // "Might need a little more"
-	local_petri_B = malloc((p_local_petri_x_dim * p_local_petri_y_dim)*sizeof(cell)); // "Might need a little more"
+	
+	local_petri_A = calloc((p_local_petri_x_dim * p_local_petri_y_dim), sizeof(cell));
+	local_petri_B = calloc((p_local_petri_x_dim * p_local_petri_y_dim), sizeof(cell));
 
-	for (int i = 0; i < p_local_petri_x_dim*p_local_petri_y_dim; i++) {
+/*	for (int i = 0; i < p_local_petri_x_dim*p_local_petri_y_dim; i++) {
 
 		local_petri_A[i].strength = 0;
 		local_petri_A[i].color = 0;
 
+	}*/
+
+//	memcpy(local_petri_B, local_petri_A, (p_local_petri_x_dim*p_local_petri_y_dim)*sizeof(cell)); 
+
+	local_petri_A_image = malloc(p_local_petri_x_dim*sizeof(cell*));
+	for (int i = 0; i < p_local_petri_x_dim; i++) {
+		local_petri_A_image[i] = &local_petri_A[(p_local_petri_x_dim * i)];
 	}
+
+	local_petri_B_image = malloc(p_local_petri_x_dim*sizeof(cell*));
+	for (int i = 0; i < p_local_petri_x_dim; i++) {
+		local_petri_B_image[i] = &local_petri_B[(p_local_petri_x_dim * i)];
+	}
+
 
 
   // TODO: Randomly perturb the local dish. Only perturb cells that belong to your process,
@@ -214,19 +231,6 @@ void initialize(){
 
 	srand(rank);
 
-	memcpy(local_petri_B, local_petri_A, (p_local_petri_x_dim*p_local_petri_y_dim)*sizeof(cell)); 
-
-	cell** local_petri_A_image = malloc(p_local_petri_x_dim*sizeof(cell*));
-	for (int i = 0; i < p_local_petri_x_dim; i++) {
-		local_petri_A_image[i] = &local_petri_A[(p_local_petri_x_dim * i)];
-	}
-
-	cell** local_petri_B_image = malloc(p_local_petri_x_dim*sizeof(cell*));
-	for (int i = 0; i < p_local_petri_x_dim; i++) {
-		local_petri_B_image[i] = &local_petri_B[(p_local_petri_x_dim * i)];
-	}
-
-	
 	// "Randomly" seeding the local dish.
 	for (int i = 0; i < p_local_petri_x_dim*2; i++) {
 
@@ -239,7 +243,6 @@ void initialize(){
 
 	} 
 
-//	printf("My dims are: %d %d\n", p_my_y_dim, p_my_x_dim);
 
 	// Setting all borders to white
 
@@ -328,7 +331,7 @@ void exchange_borders(){
 void iterate_CA(){
   //TODO: Iterate the cellular automata one step
 
-		
+//	iterate_image(local_petri_A_image, local_petri_B_image);
 
 }
 
